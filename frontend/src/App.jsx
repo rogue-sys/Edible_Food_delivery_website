bf42060eafd3bd80f2cf89aa30ac3c5da2d2765e
aadhil <adhilmuhammed06@gmail.com>
2024-06-29 18:52:51 +0530

    initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from './components/Navbar/Navbar'
+
+const App = () => {
+  return (
+    <div clasName='app'>
+      <Navbar/>
+    </div>
+  )
+}
+
+export default App
