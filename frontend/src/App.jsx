b4608c0698fc412cd01c70a0e5274be0d8f16b31
aadhil <adhilmuhammed06@gmail.com>
2024-09-10 11:56:06 +0530

    second commit after adding food items and header components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home/home'
+import Cart from './pages/Cart/cart'
+import PlaceOrder from './pages/placeOrder/placeOrder'
 
 const App = () => {
   return (
-    <div clasName='app'>
+    <div className='app'>
       <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/cart' element={<Cart/>} />
+        <Route path='/order' element={<PlaceOrder/>} />
+      </Routes>
     </div>
   )
 }
---
+++ frontend/src/pages/placeOrder/placeOrder.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './placeOrder.css'
+const PlaceOrder = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default PlaceOrder
